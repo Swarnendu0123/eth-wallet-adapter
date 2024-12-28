abaa16bd31617f889388b72ff8bd68fa0c8394e7
Swarnendu <clickswarnendu123@gmail.com>
2024-12-28 23:11:22 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  http,
+  createConfig,
+  WagmiProvider,
+  useAccount,
+  useBalance,
+  useSendTransaction,
+} from "wagmi";
+import { useConnect } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+
+const config = createConfig({
+  chains: [mainnet],
+  connectors: [injected()],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <EthSend />
+        <br />
+        ______________________________________
+        <br />
+        <WalletOptions />
+        <MyAddress />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+function MyAddress() {
+  const { address } = useAccount();
+  const { balance } = useBalance({ address });
+
+  return (
+    <div>
+      <h3>Address: {address}</h3>
+      <p>Balance: {balance?.data?.formatted}</p>
+    </div>
+  );
+}
+
+function WalletOptions() {
+  const { connectors, connect } = useConnect();
+
+  return connectors.map((connector) => (
+    <button key={connector.uid} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ));
+}
+
+function EthSend() {
+  const { data: hash, sendTransaction } = useSendTransaction();
+  const [receiverAddress, setReceiverAddress] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const sendEtherium = () => {
+    sendTransaction({
+      to: receiverAddress,
+      value: (1000000000000000000 * amount).toString(),
+    });
+  };
+
+  
+  
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Receiver Address"
+        onChange={(e) => setReceiverAddress(e.target.value)}
+      />
+      <br />
+      <input
+        type="number"
+        placeholder="Amount of ETH"
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <br />
+      <button onClick={sendEtherium}>Send</button>
+    </div>
+  );
+}
+
+export default App;
